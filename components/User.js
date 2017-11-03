79abea2a7dc0adf6ab7616487b7f119e116a8348
Rajsek <rajisekar.d@gmail.com>
2017-11-03 11:10:55 +0530

    Added ShouldComponentUpdate in Component

--- components/User.js
+++ components/User.js
@@ -33,3 +33,24 @@ export class Component extends React.Component {
     );
   }
 }
+
+export class ComponentWithShouldComponentUpdate extends React.Component {
+
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.name !== this.props.name ||
+      nextProps.age !== this.props.age ||
+      nextProps.hobby !== this.props.hobby;
+  }
+
+  render() {
+    const { name, age, hobby } = this.props;
+    return (
+      <div>
+        <span>{name}</span>
+        <span>{age}</span>
+        <span>{hobby}</span>
+      </div>
+    );
+  }
+}
